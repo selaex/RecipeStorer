1280d9e9e8325a39fcc031a512ce5235928f24e4
Peter Fewster <peter@peters-macbook-pro.local>
2018-04-30 19:08:50 +1000

    Project start

---
+++ RecipeStorer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RecipeStorer
+//
+//  Created by Peter Fewster on 30/4/18.
+//  Copyright © 2018 Deakin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
